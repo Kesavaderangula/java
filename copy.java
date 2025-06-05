6e556fd633d96cb427eb18c8297cf4acab6d89ab
Kesavaderangula <derangulakesava76@gmail.com>
2025-06-05 15:42:39 +0530

    Add files via upload

---
+++ copy.java
@@ -0,0 +1,18 @@
+import java.util.*;
+  class copy {    
+   public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int []a=new int[5];
+     for (int i=0;i<a.length;i++)
+    {
+           a[i]=sc.nextInt();
+        }
+		b[i]=sc.next[a.length];
+		b[i]=a[i];
+		b[0]++;
+        for (int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]+" ");
+        }
+    }
+}
